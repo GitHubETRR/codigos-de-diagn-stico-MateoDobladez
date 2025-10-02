3808d6fcfd144568214acff4a12d575a94cbfb64
MateoDobladez <mateodobla@gmail.com>
2025-10-02 12:39:00 -0300

    codigo nuevo python

---
+++ codigo_prueba.py
@@ -0,0 +1,15 @@
+def main() -> None:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    suma = num1 + num2
+    promedio = suma / 2.0
+
+    print(f"La suma es: {suma}")
+    print(f"El promedio es: {promedio}")
+
+
+if __name__ == "__main__":
+    main()
+
+
